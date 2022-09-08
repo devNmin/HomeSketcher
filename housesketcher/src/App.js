0fabe6f3b17d0a947f49d1dbb6bb18aff56e44a9
윤영훈 <rod3539@hanmail.net>
2022-09-08 14:43:19 +0900

    loginmainpage carousel

--- housesketcher/src/App.js
+++ housesketcher/src/App.js
@@ -1,11 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import NonLoginMainPage from './pages/NonLoginMainPage';
+// import NonLoginMainPage from './pages/NonLoginMainPage';
+import LoginMainPage from './pages/LoginMainPage';
 
 function App() {
   return (
     <div className="App">
-      <NonLoginMainPage />
+      <LoginMainPage />
     </div>
   );
 }
---
+++ housesketcher/src/pages/LoginMainPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import MainCarousel from '../components/LoginMainPage/MainCarousel'
+
+function LoginMainPage(props) {
+	return (
+		<>
+			<Navbar />
+			<MainCarousel />
+		</>
+	);
+}
+
+export default LoginMainPage;
