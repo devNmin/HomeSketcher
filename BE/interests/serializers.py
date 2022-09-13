97684d53eea7e28a702366c4491ce7e1d43bb896
kimjinsan <lazybones1@naver.com>
2022-09-13 14:03:10 +0900

    FEAT: 유저 취향 분석 결과
    유저 취향 분석 결과 및 리턴 타입 수정

--- BE/interests/serializers.py
+++ BE/interests/serializers.py
@@ -23,12 +23,15 @@ class InterestStyleInputSerializer(serializers.ModelSerializer):
 class InterestColorInputSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserColor
-        fields = fields = [
+        fields = [
             'color_name',
             'color_cnt'
         ]
-         
-        
+
+class UserInterestDataSerializer(serializers.Serializer):
+    style = serializers.CharField(max_length=30, default=0)
+    color = serializers.CharField(max_length=30, default=0)
+          
 # 취향 폼 작성 결과 swagger
 class InterestFormResultSerialiizer(serializers.Serializer):
     img_list = serializers.ListField(
--- BE/interests/urls.py
+++ BE/interests/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import(
     UserInterestsFormAPIView,
     UserInterestResult,
+    SendUserInterestResult,
 )
 
 app_name = 'interests'
@@ -9,4 +10,5 @@ app_name = 'interests'
 urlpatterns = [
     path('data/', UserInterestsFormAPIView.as_view(), name="data"),
     path('user/', UserInterestResult.as_view(), name="user"),
+    path('user/<user_pk>', SendUserInterestResult.as_view(), name="user"),
 ]
