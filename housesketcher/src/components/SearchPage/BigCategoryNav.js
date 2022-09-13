9039d022226fc23329f7eb8b809f269e16e812ea
le2sk <tmfrl935@naver.com>
2022-09-13 17:28:14 +0900

    chip component 구현, 핉터ing

--- housesketcher/src/components/SearchPage/BigCategoryNav.js
+++ housesketcher/src/components/SearchPage/BigCategoryNav.js
@@ -1,43 +1,41 @@
 import { Link } from 'react-router-dom';
+import classes from './BigCategoryNav.module.css';
 
 function BigCategoryNav() {
   return (
-    <header>
-      <div>React Meetups</div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="">Sofas</Link>
-          </li>
-          <li>
-            <Link to="">Beds</Link>
-          </li>
-          <li>
-            <Link to="">Tables</Link>
-          </li>
-          <li>
-            <Link to="">Chairs</Link>
-          </li>
-          <li>
-            <Link to="">Media</Link>
-          </li>
-          <li>
-            <Link to="">Shelves</Link>
-          </li>
-          <li>
-            <Link to="">Closets</Link>
-          </li>
-          <li>
-            <Link to="">Kids</Link>
-          </li>
-          <li>
-            <Link to="">Storage</Link>
-          </li>
-          <li>
-            <Link to="">Lamps</Link>
-          </li>
-        </ul>
-      </nav>
+    <header className={classes.header}>
+      <ul>
+        <li>
+          <Link to="">Sofas</Link>
+        </li>
+        <li>
+          <Link to="">Beds</Link>
+        </li>
+        <li>
+          <Link to="">Tables</Link>
+        </li>
+        <li>
+          <Link to="">Chairs</Link>
+        </li>
+        <li>
+          <Link to="">Media</Link>
+        </li>
+        <li>
+          <Link to="">Shelves</Link>
+        </li>
+        <li>
+          <Link to="">Closets</Link>
+        </li>
+        <li>
+          <Link to="">Kids</Link>
+        </li>
+        <li>
+          <Link to="">Storage</Link>
+        </li>
+        <li>
+          <Link to="">Lamps</Link>
+        </li>
+      </ul>
     </header>
   );
 }
