59cb3db7ae6f8408faf619f4f1272c81f118005d
khlzzang <khlzzang1104@gmail.com>
2022-09-14 13:29:36 +0900

    FIX: useContext Error fixed

--- housesketcher/src/pages/LoginMainPage.js
+++ housesketcher/src/pages/LoginMainPage.js
@@ -1,14 +1,13 @@
-import React , {useContext }from 'react';
+import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import MainCarousel from '../components/LoginMainPage/MainCarousel'
 import SubCarousel from '../components/LoginMainPage/SubCarousel'
 import Footer from '../components/Footer/Footer';
-import AuthContext from '../context/AuthContext';
+
 
 // import styles from './LoginMainPage.module.css'
 
-function LoginMainPage(props) {
-	let {user} = useContext(AuthContext)
+function LoginMainPage(props) {	
 	return (
 		<>
 			<Navbar />
