d04cf0791e177706300d12bcc0e1514ce54fe580
seulgiL <seulki199@gmail.com>
2022-09-15 17:57:45 +0900

    [FEAT] filter login -ing

--- housesketcher/src/store/search.js
+++ housesketcher/src/store/search.js
@@ -1,14 +1,59 @@
 import { createContext, useState } from 'react';
 
+/* 
+page*	integer
+title: Page
+페이지 번호 0부터 줘야함
+
+main*	string
+title: Main
+minLength: 1
+가구 대분류
+
+sub*	string
+title: Sub
+minLength: 1
+가구 소분류
+
+minPrice	number
+title: Minprice
+가구 최소 가격. 없으면 null
+
+maxPrice	number
+title: Maxprice
+가구 최대 가격. 없으면 null
+
+width	number
+title: Width
+가구 가로 길이
+
+length	number
+title: Length
+가구 세로 길이
+
+height	number
+title: Height
+가구 높이
+
+*/
+
 const FilterContext = createContext({
   categotyFilters: [],
   addFilter: (filterName) => {},
   removeFilter: (filterName) => {},
   isSelectedFilter: (filterName) => {},
+  changePage: (pageNum) => {},
+  changeBigCategory: (categoryName) => {},
 });
 
 export function FilterContextProvider(props) {
   const [selectedFilters, setSelectedFilters] = useState([]);
+  const [page, setPage] = useState([0]);
+  const [main, setMain] = useState('Sofas & sectionals');
+
+  const setPageHandler = (pageNum) => {
+    setPage(pageNum);
+  };
 
   const addFilterHandler = (filterName) => {
     setSelectedFilters((prevList) => [...prevList, filterName]);
@@ -23,7 +68,9 @@ export function FilterContextProvider(props) {
   }
 
   function IsSelectedFilterHandler(filterName) {
-    return selectedFilters.some((categoryFilter) => categoryFilter === filterName);
+    return selectedFilters.some(
+      (categoryFilter) => categoryFilter === filterName
+    );
   }
 
   const context = {
@@ -31,10 +78,13 @@ export function FilterContextProvider(props) {
     addFilter: addFilterHandler,
     removeFilter: removeFilterHandler,
     isSelectedFilter: IsSelectedFilterHandler,
+    changePage: setPageHandler,
   };
 
   return (
-    <FilterContext.Provider value={context}>{props.children}</FilterContext.Provider>
+    <FilterContext.Provider value={context}>
+      {props.children}
+    </FilterContext.Provider>
   );
 }
 
