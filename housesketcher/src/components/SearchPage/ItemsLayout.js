123bd73ef9ddbe69b90cb16e848bbc07f5611b1e
seulgiL <seulki199@gmail.com>
2022-09-19 17:36:40 +0900

    [CHORE] delete searchcontext copy

--- housesketcher/src/components/SearchPage/ItemsLayout.js
+++ housesketcher/src/components/SearchPage/ItemsLayout.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from 'react';
+import { useContext } from 'react';
 import SearchContext from '../../context/SearchContext';
 function ItemsLayout() {
   const filterCtx = useContext(SearchContext);
