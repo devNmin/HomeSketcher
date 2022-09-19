63b6cbc431c7493ad27affb21c54466d158d6a72
kimjinsan <lazybones1@naver.com>
2022-09-19 17:14:20 +0900

    FEAT: fix style list data
    대소문자 오류 수정

--- BE/util/choicesList.py
+++ BE/util/choicesList.py
@@ -4,11 +4,11 @@ select_style = (
     ('Natural','Natural'),
     ('Minimal','Minimal'),
     ('North European','North European'),
-    ('Vintage','Vintage'),
-    ('Antique','Antique'),
+    ('vintage','vintage'),
+    ('antique','antique'),
     ('Provence','Provence'),
-    ('Provence&Romantic','Provence&Romantic'),
-    ('Mediterranean Sea','Mediterranean Sea'),
+    ('provence&romantic','provence&romantic'),
+    ('mediterranean','mediterranean'),
 )
 #선호 색상
 select_color = (
@@ -32,5 +32,5 @@ select_color_sub = (
 )
 
 category = {'Shelves': ['Buffet tables, sideboard buffets & buffet cabinets', 'Bookcases', 'Display cabinets', 'Shelf units & cube storage', 'Dining room cabinets & cupboards', 'Storage shelves & units', 'Filing cabinets & storage cabinets', 'Cube storage', 'Kitchen pantry storage'], 'Sofas': ['Sofa & armchairs covers', 'Fabric sofas', 'Leather & faux leather sofas', 'Ottomans', 'Sectionals', 'Sofa legs', 'Sofa accessories'], 'Media': ['TV stands & benches', 'TV & media storage'], 'Tables': ['Desks & computer desks', 'Dining sets', 'Dining tables', 'Accent tables', 'Nightstands', 'Changing tables', 'Kids tables', 'Café tables', 'Makeup vanities & dressing tables', 'BEKANT conference & meeting tables', 'Console tables & sofa tables', 'Bar tables'], 'Beds': ['Full, Queen and King size platform beds', 'Sleeper sofas', 'Twin beds', 'Kids beds', 'Headboards', 'Under bed storage', 'Cribs', 'Bed legs', 'Beds with storage', 'Guest beds & daybeds', 'Upholstered beds'], 'Chairs': ['Desk chairs', 'Dining chairs', 'Bar stools & chairs', 'Chair pads', 'High chairs', 'Stools & benches', 'Café chairs', 'Step stools & step ladders'], 'Closets': ['Chests of drawers', 'Open wardrobes', 'Drawer units', 'Solitaire wardrobes', 'Closet lighting', 'Hinged wardrobes', 'Walk in wardrobes', "Children's dressers and chests", "Children's wardrobes", 'Sliding wardrobes', 'Fitted wardrobes'], 'Lamps': ['Ceiling lights', 'Shades, bases & cords', 'Floor lamps', 'Table lamps', 'Kids lighting', 'Desk lamps', 'Wall lamps'], 'Kids': ['Toy storage', 'Kids chairs', 'Kids stools & benches']}
-style = ['Modern', 'Natural', 'Minimal', 'North European', "Vintage", "Antique", "Provence&Romantic", "Mediterranean Sea"]
+style = ['Modern', 'Natural', 'Minimal', 'North European', "vintage", "antique", "provence&romantic", "mediterranean"]
 color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple', 'brown', 'pink', 'white', 'black', 'gray']
