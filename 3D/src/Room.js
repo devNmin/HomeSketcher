a9727802304c0fa7bca75e031abb52f26e7bf335
조경민 <21701052@handong.edu>
2022-09-26 17:32:08 +0900

    feat:3d-modeling create room
    되라 제발 ㅋㅋ

---
+++ 3D/src/Room.js
@@ -0,0 +1,60 @@
+import React, { useMemo } from "react";
+import { pointIsOnSegment } from "./geometry";
+
+import Flooring from "./Flooring";
+import Wall from "./Wall";
+import Corner from "./Corner";
+
+const Room = ({
+  id,
+  coords,
+  height,
+  doors,
+  windows,
+  showCorners = true,
+  isInteractive = true
+}) => {
+  const walls = useMemo(
+    () =>
+      coords.map((_, i) => {
+        const a = coords[i];
+        const b = coords[(i + 1) % coords.length];
+        return {
+          a,
+          b,
+          height,
+          doors: doors.filter((door) => doorIsOnWall(door, [a, b])),
+          windows: windows.filter((window) => windowIsOnWall(window, [a, b]))
+        };
+      }),
+    [coords, height, doors, windows]
+  );
+
+  return (
+    <group>
+      {walls.map((wall) => (
+        <Wall
+          key={`${id}-wall-[${wall.a.x},${wall.a.y}],[${wall.b.x},${wall.b.y}]`}
+          {...wall}
+          isInteractive={isInteractive}
+        />
+      ))}
+      <Flooring coords={coords} isInteractive={isInteractive} />
+      {showCorners &&
+        coords.map(({ x, y: z }) => {
+          let top = [x, height, z];
+          let bottom = [x, 0, z];
+          return [
+            <Corner key={`top-corner-${x},${height},${z}`} coord={top} />,
+            <Corner key={`bottom-corner-${x},0,${z}`} coord={bottom} />
+          ];
+        })}
+    </group>
+  );
+};
+
+const doorIsOnWall = ({ hinge }, wall) => pointIsOnSegment(hinge, wall);
+
+const windowIsOnWall = ({ left }, wall) => pointIsOnSegment(left, wall);
+
+export default Room;
