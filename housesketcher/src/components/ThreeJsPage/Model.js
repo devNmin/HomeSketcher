3ec0a425d4ec7bfce5a6428ea22d3e06b8c82a3b
khlzzang <khlzzang1104@gmail.com>
2022-09-28 00:45:39 +0900

    FEAT: 조경민 뒤를 부탁한다

--- housesketcher/src/components/ThreeJsPage/Model.js
+++ housesketcher/src/components/ThreeJsPage/Model.js
@@ -1,25 +1,16 @@
 // npx gltfjsx pot.glb
 import React , { useState } from 'react';
 import { useGLTF, useCursor} from '@react-three/drei';
-
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import create from 'zustand'
 
-const useStore = create((set) => ({ target: null, setTarget: (target) => set({ target }) }))
-export function Model(props) {
-  const { nodes, materials } = useGLTF('/pot.glb');
-  const setTarget = useStore((state) => state.setTarget)
+// const useStore = create((set) => ({ target: null, setTarget: (target) => set({ target }) }))
 
-  const [hovered, setHovered] = useState(false)
-  useCursor(hovered)
-  return (
-      <mesh {...props} onClick={(e) => setTarget(e.object)} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)}
-      // <mesh {...props} onClick={(e) => cc(e)} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)}
-        geometry={nodes.normalized_model.geometry}
-        material={materials.solid_001_wire}
-        rotation={[Math.PI / 2, 0, 0]}
-      >
-      </mesh>
+export default  function Model (props)  {
+  const gltf = useLoader(
+    GLTFLoader,
+    props.objUrl
   );
+  return <primitive object={gltf.scene} scale={0.4} />;
 }
-
-useGLTF.preload('/pot.glb');
--- housesketcher/src/pages/ThreeJsPage.js
+++ housesketcher/src/pages/ThreeJsPage.js
@@ -1,4 +1,4 @@
-import React, {useState, useReducer, useMemo, useRef } from 'react';
+import React, {useState, useReducer, useMemo, useRef, Suspense } from 'react';
 import { Canvas, useThree, useFrame  } from "react-three-fiber";
 import { a, useSpring } from '@react-spring/three';
 // import data from '../components/ThreeJsPage/floplan-data.json';
@@ -6,15 +6,18 @@ import CameraSetup from '../components/ThreeJsPage/CameraSetup';
 import FloorPlan from '../components/ThreeJsPage/FloorPlan';
 import FloorClip from '../components/ThreeJsPage/FloorClip';
 
-import { Model } from '../components/ThreeJsPage/Model';
+import  Model  from '../components/ThreeJsPage/Model';
 import { DISTANCE_BETWEEN_FLOORS } from '../components/ThreeJsPage/constants';
 import classes from './ThreeJsPage.module.css';
 
 ///////////////////////
+import { useLoader } from "@react-three/fiber";
 import create from 'zustand'
-import { OrbitControls, TransformControls,  } from '@react-three/drei'
+import { Environment, OrbitControls, TransformControls, Html, useProgress } from '@react-three/drei'
 import { useControls } from 'leva'
 import { useGLTF, useCursor} from '@react-three/drei';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import Liked from '../components/ThreeJsPage/Liked';
 
 //////////////////////
 
@@ -27,44 +30,70 @@ const DevTools = () => {
   return null;
 };
 
+// 1. Glb 파일 로딩 
+// function Loader() {
+//   const { active, progress, errors, item, loaded, total } = useProgress();
+//   return <Html center>{progress} % loaded</Html>;
+// }
 
+// const Model = () => {
+//   const gltf = useLoader(
+//     GLTFLoader,
+//     'https://firebasestorage.googleapis.com/v0/b/homesketcher-37070.appspot.com/o/glb%2Fdesks.glb?alt=media&token=eyJhbGciOiJSUzI1NiIsImtpZCI6IjIxZTZjMGM2YjRlMzA5NTI0N2MwNjgwMDAwZTFiNDMxODIzODZkNTAiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoia21TZWNvbmQiLCJwaWN0dXJlIjoiaHR0cDovL3d3dy5leGFtcGxlLmNvbS8xMjM0NTY3OC9waG90by5wbmciLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vaG9tZXNrZXRjaGVyLTM3MDcwIiwiYXVkIjoiaG9tZXNrZXRjaGVyLTM3MDcwIiwiYXV0aF90aW1lIjoxNjY0MjQ2Njk5LCJ1c2VyX2lkIjoiZzVnNzJuU1ZmWWd0ZVJBQ3UzWHNTTVBDTzVGMyIsInN1YiI6Imc1ZzcyblNWZllndGVSQUN1M1hzU01QQ081RjMiLCJpYXQiOjE2NjQyNDY2OTksImV4cCI6MTY2NDI1MDI5OSwiZW1haWwiOiJqbzk1MTEyOEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsInBob25lX251bWJlciI6Iis4MjEwNjQ4NTU3OTQiLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7InBob25lIjpbIis4MjEwNjQ4NTU3OTQiXSwiZW1haWwiOlsiam85NTExMjhAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.jbsBNl9xzkPZTEuO3NID7cbC4QINFcka5o73QyUP4_rt-9V4iRUO19eVp1JVSjs-16xToS-9xZgVQ8--j9-Cv_8ihku2veGrqNoXSX2U_Z-GrPg0DcjrR0MY8oof6-ZofCjt-Dqhe1twVbD1ijmTXl28ISs8eVG8LoyVbppkvFJq5TmV1Mh5wiMz6Y4LgPSW_CcJY5RwDTapA5oxkP_psALDr1Iopz_ADZNymW_dOPbGkbOqJHqk0PgEFz6gzcvJcpDJwIZupvia71q6cdmb_tVuy2Tu7_0Uk88r_2YHnNrC3bBGxhotWCoVFF11SF_UBGztJRBI_JBYRZqL-PFuvA'
+//   );
+//   return <primitive object={gltf.scene} scale={0.4} />;
+// };
 const useStore = create((set) => ({ target: null, setTarget: (target) => set({ target }) }))
 
-function ModelT(props) {
+// 가구 3D 모델 생성?
+// function ModelT(props) {
 
-  const { nodes, materials } = useGLTF('/pot.glb');
-  const setTarget = useStore((state) => state.setTarget)
-  const [hovered, setHovered] = useState(false)
+//   const { nodes, materials } = useLoader(
+//     GLTFLoader,
+//     'https://firebasestorage.googleapis.com/v0/b/homesketcher-37070.appspot.com/o/glb%2Fdesks.glb?alt=media&token=eyJhbGciOiJSUzI1NiIsImtpZCI6IjIxZTZjMGM2YjRlMzA5NTI0N2MwNjgwMDAwZTFiNDMxODIzODZkNTAiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoia21TZWNvbmQiLCJwaWN0dXJlIjoiaHR0cDovL3d3dy5leGFtcGxlLmNvbS8xMjM0NTY3OC9waG90by5wbmciLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vaG9tZXNrZXRjaGVyLTM3MDcwIiwiYXVkIjoiaG9tZXNrZXRjaGVyLTM3MDcwIiwiYXV0aF90aW1lIjoxNjY0MjQ2Njk5LCJ1c2VyX2lkIjoiZzVnNzJuU1ZmWWd0ZVJBQ3UzWHNTTVBDTzVGMyIsInN1YiI6Imc1ZzcyblNWZllndGVSQUN1M1hzU01QQ081RjMiLCJpYXQiOjE2NjQyNDY2OTksImV4cCI6MTY2NDI1MDI5OSwiZW1haWwiOiJqbzk1MTEyOEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsInBob25lX251bWJlciI6Iis4MjEwNjQ4NTU3OTQiLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7InBob25lIjpbIis4MjEwNjQ4NTU3OTQiXSwiZW1haWwiOlsiam85NTExMjhAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.jbsBNl9xzkPZTEuO3NID7cbC4QINFcka5o73QyUP4_rt-9V4iRUO19eVp1JVSjs-16xToS-9xZgVQ8--j9-Cv_8ihku2veGrqNoXSX2U_Z-GrPg0DcjrR0MY8oof6-ZofCjt-Dqhe1twVbD1ijmTXl28ISs8eVG8LoyVbppkvFJq5TmV1Mh5wiMz6Y4LgPSW_CcJY5RwDTapA5oxkP_psALDr1Iopz_ADZNymW_dOPbGkbOqJHqk0PgEFz6gzcvJcpDJwIZupvia71q6cdmb_tVuy2Tu7_0Uk88r_2YHnNrC3bBGxhotWCoVFF11SF_UBGztJRBI_JBYRZqL-PFuvA'
+//   );;
+//   const setTarget = useStore((state) => state.setTarget)
+//   const [hovered, setHovered] = useState(false)
 
-  function clcikHandler(data){
-    console.log('------------')
-    console.log('data',data)
-    setTarget(data)
-    console.log('------------')
-    console.log('setTargetsetTargetsetTarget',data)
-    console.log('------------')
-    // console.log('target',target)
-  }
-  useCursor(hovered)
-  return (
-      // <mesh {...props} onClick={(e) => {setTarget(e.object)}} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)}
-      <mesh {...props} onClick={(e) => {setTarget(e.object); clcikHandler(e.object)}} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)}
-      // <mesh {...props} onClick={(e) => clcikHandler(e.object)} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)}
-      // <mesh {...props} onClick={(e) => cc(e)} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)}
-        geometry={nodes.normalized_model.geometry}
-        material={materials.solid_001_wire}
-        rotation={[Math.PI / 2, 0, 0]}
-      >
-      </mesh>
-  );
-}
+//   function clcikHandler(data){
+//     console.log('------------')
+//     console.log('data',data)
+//     setTarget(data)
+//     console.log('------------')
+//     console.log('setTargetsetTargetsetTarget',data)
+//     console.log('------------')
+//     // console.log('target',target)
+//   }
+//   useCursor(hovered)
+//   return (
+//       // <mesh {...props} onClick={(e) => {setTarget(e.object)}} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)}
+//       <mesh {...props} onClick={(e) => {setTarget(e.object); clcikHandler(e.object)}} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)}
+//       // <mesh {...props} onClick={(e) => clcikHandler(e.object)} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)}
+//       // <mesh {...props} onClick={(e) => cc(e)} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)}
+//         geometry={nodes.normalized_model.geometry}
+//         material={materials.solid_001_wire}
+//         rotation={[Math.PI / 2, 0, 0]}
+//       >
+//       </mesh>
+//   );
+// }
 
 export default function ThreeJsPage() {
   // let [currentFloor, setCurrentFloor] = useState(0);
   let currentFloor= 0;
   let [showCorners, setShowCorners] = useState(false);
   let [orthoCamera, setOrthoCamera] = useState(false);
+  let [objList, setObjList] = useState([])
 
+
+  // 가구 obj 더해주기 
+  const addobjListHandler = (objUrl) => {
+    setObjList((prevObjList) => {
+      return [...prevObjList, objUrl]
+    })
+  }
+
+  
   let [X, setX] = useState(0);
   let [Y, setY] = useState(0);
   let [H, setH] = useState(0);
@@ -141,22 +170,28 @@ export default function ThreeJsPage() {
   return (
     <div className={classes.three_body}>
 
+        {/* 가구 UX 창 */}
         <div className={classes.LeftItems}>
           <div>
-            HI
-            {/* <h1>{target}</h1> */}
+            <Liked addObj = {addobjListHandler}/>          
             <h1>{mode}</h1>
           </div>
         </div>
+
         <div className={classes.RightItems}>
         <Canvas  onPointerMissed={() => setTarget(null)} 
           key={`isometric-${orthoCamera}`}
           orthographic={orthoCamera}
           invalidateframeloop="false">
-          
-          <ModelT/>
-          {/* <CameraSetup /> */}
-          
+
+          {/* 가구 3D 모델 */}
+          {objList.map((obj) => (
+            <Model objUrl = {obj}/>
+          ))
+          }
+
+
+          {/* <CameraSetup /> */}          
           <ambientLight intensity={0.5} color="#eef" />
           <pointLight position={[20, 10, -10]} decay={1} castShadow={true} />
           <pointLight position={[-20, 20, 5]} decay={1} castShadow={true} />
@@ -176,6 +211,7 @@ export default function ThreeJsPage() {
         </Canvas>
       <div>
         
+        {/* 뷰 + 코너 확인 */}
         <div className={`${classes.controls} ${classes.perspectiveControls}`}>
           <div>
             <label htmlFor="isometricView">Isometric View</label>
@@ -197,6 +233,8 @@ export default function ThreeJsPage() {
           </div>
         </div>
 
+
+        {/* 방 수치 입력 */}
         <div className={classes.help} >
           <form>
             <label htmlFor="xx">X</label>
@@ -207,6 +245,8 @@ export default function ThreeJsPage() {
             <input id="hh" ref={HHH} onChange={changeHHandler} />
           </form>
         </div>
+
+
 
         <div className={`${classes.controls} ${classes.doorControls}`}>
           {newItem.floors[currentFloor].doors
