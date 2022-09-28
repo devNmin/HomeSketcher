2778c5f843c1f7360c8279a752ecc2819f90f591
조경민 <21701052@handong.edu>
2022-09-28 16:05:15 +0900

    merge: fastapi

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,30 +1,32 @@
 from fastapi import FastAPI , File, UploadFile
 from predict import predict_img
 import numpy as np
+from PIL import Image
+import shutil
+
 app = FastAPI()
 
 @app.get("/fastapi/")
 def read_root():
     return {"Hello": "World"}
-    
-@app.post("/fastapi/uploadfiles/")
-async def create_upload_files(files: list[UploadFile]):
-    
-    return {"filenames": [file.filename for file in files]}
 
-# @app.get("/fastapi/predict")
-# def read_root():
-#     res = []
-#     img_path = 'img/ttt.jpg'
-#     return_value = predict_img(img_path)
-#     print(type(return_value))
-#     arr = np.array(return_value)
-#     print('-------------')
-#     print(arr)
-#     print(type(arr.tolist()))
-#     print('-------------')
-#     print(arr)
+@app.post("/fastapi/predict")
+async def predict( image: UploadFile = File(...)):
+    class_names = ['class_antique', 'class_mediterranean', 'class_natural']
+    res = {}
+    path = f'img/{image.filename}'
 
-#     res.append(float(arr[0][0]))
-#     return res
-    
+    with open(path, "wb") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+    # AI model 
+    return_value = predict_img(path)
+
+    arr = np.array(return_value)
+
+    res = { }
+    class_names = ['Antique', 'Mediterranean', 'Natural']
+    for i in range(len(class_names)):
+        res[class_names[i]] = round(arr[0][i]*100,2)
+    result = dict(sorted(res.items(), key = lambda res: res[1],reverse=True))
+
+    return {"predict":result}
